3922e1c849da810fba3b380b68276e0a4798be45
Geomario <marm1984@gmail.com>
2019-01-23 09:30:29 -0600

    Uptodate python script with the graphs in bokeh for all the stocks & result in html

--- geostock.py
+++ geostock.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from bokeh.plotting import figure,output_file, show
 from bokeh.themes import built_in_themes
 from bokeh.io import curdoc
+from bokeh.models import Legend
 
 yf.pdr_override() #To fix the downloading in DataReader
 
@@ -30,7 +31,7 @@ for stock in stock_tickers:
 
 #Creating a big dataframe for all the stocks
 df_listings = pd.concat(listings)
-# print (df_listings.head()) #Checking the shape of only one DF
+print (df_listings.head()) #Checking the shape of only one DF
 
 #Graphs in matplotlib
 listings[1]["Close"].plot()
@@ -39,6 +40,18 @@ listings[1]["Close"].plot()
 #Graph in bokeh
 curdoc().theme ="dark_minimal"
 p = figure(plot_width=980, plot_height=390, x_axis_type="datetime", title = "Geothermal Stocks")
-p.line(listings[1].index, listings[1]["Close"] ,color="greenyellow", alpha=0.5)
+
+l0 = p.line(listings[0].index, listings[0]["Close"] ,color="greenyellow", alpha=0.5) # HTM
+l1 = p.line(listings[1].index, listings[1]["Close"], color="red", alpha=0.5) #ORA
+# l2 = p.line(listings[2].index, listings[2]["Close"] ,color="deepskyblue", alpha=0.5) #CVX most expensive
+l3 = p.line(listings[3].index, listings[3]["Close"] ,color="aqua", alpha=0.5) #BRK-A
+l4 = p.line(listings[4].index, listings[4]["Close"] ,color="deeppink", alpha=0.5) #ENEL.MI
+l5 = p.line(listings[5].index, listings[5]["Close"] ,color="gold", alpha=0.5) #PIF.TO
+l6 = p.line(listings[6].index, listings[6]["Close"] ,color="ivory", alpha=0.5) #INE.TO
+# l7 = p.line(listings[7].index, listings[7]["Close"] ,color="yellow", alpha=0.5) #LXV
+# p.line(listings[8].index, listings[8]["Close"] ,color="aqua", alpha=0.5) #CLIME-B.ST
+
+legend1 = Legend(items=[("HTM", [l0]),("ORA", [l1]), ("BRK-A", [l3]), ("ENEL.MI", [l4]), ("PIF.TO", [l5]), ("INE.TO", [l6]) ], location=(70,20), orientation="horizontal")
+p.add_layout(legend1, "below")
 output_file("geostock.html")
 show(p)
